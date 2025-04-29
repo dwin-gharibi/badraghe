837ad4a69925ab908d5d1b9aa2c8a209af033a2b
Dwin Gharibi <dwin.gharibi@email.kntu.ac.ir>
2025-04-29 21:15:05 +0330

    feat(badraghe-queries.sql): adding and testing query 2

--- badraghe-queries.sql
+++ badraghe-queries.sql
@@ -5,3 +5,10 @@ WHERE NOT EXISTS (
     SELECT 1 FROM user_reservations ur 
     WHERE ur.user_id = u.id
 );
+-- Query 2
+SELECT u.first_name, u.last_name
+FROM users u
+WHERE EXISTS (
+    SELECT 1 FROM user_reservations ur 
+    WHERE ur.user_id = u.id
+);
