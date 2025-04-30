3adb1483dfb0c4327397d14dc019c8310cb26d23
Dwin Gharibi <dwin.gharibi@email.kntu.ac.ir>
2025-04-30 21:27:18 +0330

    feat(badraghe-stored-procedures.sql): adding and testing procedures 3

--- badraghe-stored-procedures.sql
+++ badraghe-stored-procedures.sql
@@ -1,3 +1,4 @@
+-- Stored Procedure 1
 DELIMITER //
 
 CREATE PROCEDURE GetUserTickets(IN user_contact VARCHAR(100))
@@ -10,4 +11,19 @@ BEGIN
     JOIN travel_tickets tt ON ur.ticket_id = tt.id
     WHERE u.email = user_contact OR u.phone = user_contact
     ORDER BY ur.reserved_at;
+END //
+
+-- Stored Procedure 3
+DELIMITER //
+
+CREATE PROCEDURE GetTicketsByCity(IN city_name VARCHAR(100))
+BEGIN
+    SELECT 
+        tt.*,
+        u.first_name,
+        u.last_name
+    FROM travel_tickets tt
+    JOIN user_reservations ur ON tt.id = ur.ticket_id
+    JOIN users u ON ur.user_id = u.id
+    WHERE tt.departure_city = city_name;
 END //
