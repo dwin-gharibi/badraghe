589475b55edb7742a6ded2ae1c4fdcd1af0b562f
Dwin Gharibi <dwin.gharibi@email.kntu.ac.ir>
2025-04-30 21:35:03 +0330

    feat(badraghe-stored-procedures.sql): adding and testing procedures 8

--- badraghe-stored-procedures.sql
+++ badraghe-stored-procedures.sql
@@ -27,3 +27,26 @@ BEGIN
     JOIN users u ON ur.user_id = u.id
     WHERE tt.departure_city = city_name;
 END //
+
+-- Stored Procedure 8
+DELIMITER //
+
+CREATE PROCEDURE GetUsersWithMostReports(IN report_subject VARCHAR(50))
+BEGIN
+    CREATE TEMPORARY TABLE temp_user_reports AS
+    SELECT 
+        u.id,
+        u.first_name,
+        u.last_name,
+        COUNT(*) AS report_count
+    FROM users u
+    JOIN reports r ON u.id = r.user_id
+    WHERE r.category = report_subject
+    GROUP BY u.id;
+    
+    SELECT first_name, last_name, report_count
+    FROM temp_user_reports
+    ORDER BY report_count DESC;
+    
+    DROP TEMPORARY TABLE temp_user_reports;
+END //
