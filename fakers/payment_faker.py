6032adea24977738f5bafc215c585da4d2617905
Dwin Gharibi <dwin.gharibi@email.kntu.ac.ir>
2025-05-06 08:15:28 +0330

    fix(payment_faker): fixing and generalizing payment fakers

--- fakers/payment_faker.py
+++ fakers/payment_faker.py
@@ -11,11 +11,11 @@ def fake_payment(fake=None, user_ids=None, reservation_ids=None, method_ids=None
     return {
         "user_id": random.choice(user_ids),
         "reservation_id": random.choice(reservation_ids),
-        "amount": round(random.uniform(50, 1000), 2),
+        "amount": round(random.uniform(50, 1000), 2) * 10000000,
         "payment_method_id": random.choice(method_ids),
         "status": random.choice(statuses),
         "transaction_id": str(uuid.uuid4()),
-        "currency": "USD",
-        "refund_amount": round(random.uniform(0, 200), 2),
+        "currency": "IRR",
+        "refund_amount": round(random.uniform(0, 200), 2) * 1000000,
         "payment_details": None
     }
