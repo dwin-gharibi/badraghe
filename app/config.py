3792e5cef681ca657a569ec6b0e622dc0a4e08c0
Dwin Gharibi <dwin.gharibi@email.kntu.ac.ir>
2025-05-30 21:57:53 +0330

    feat(app/config.py): adding config file for badraghe api core configs

---
+++ app/config.py
@@ -0,0 +1,21 @@
+import os
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    mysql_host: str = os.getenv("MYSQL_HOST")
+    mysql_user: str = os.getenv("MYSQL_USER")
+    mysql_port: int = os.getenv("MYSQL_PORT")
+    mysql_password: str = os.getenv("MYSQL_PASSWORD")
+    mysql_db: str = os.getenv("MYSQL_DB")
+
+    redis_host: str = os.getenv("REDIS_HOST")
+    redis_port: int = int(os.getenv("REDIS_PORT"))
+    redis_db: int = int(os.getenv("REDIS_DB"))
+
+    jwt_expiration_minutes: int = int(os.getenv("JWT_EXPIRATION_MINUTES"))
+    jwt_secret: str = os.getenv("JWT_SECTET")
+    jwt_algorithm: str = os.getenv("JWT_ALGORITHM")
+
+    otp_expiration_seconds: int = 300
+
+settings = Settings()
