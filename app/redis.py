fb0e3e7f5633237ff2622a6d80b84cf6b3367516
Dwin Gharibi <dwin.gharibi@email.kntu.ac.ir>
2025-05-30 06:37:34 +0330

    feat(app/redis.py): adding redis client to getting touch with badraghe redis core

---
+++ app/redis.py
@@ -0,0 +1,20 @@
+from redis.asyncio import Redis
+from app.config import settings
+
+redis_client: Redis | None = None
+
+async def get_redis() -> Redis:
+    global redis_client
+    if redis_client is None:
+        redis_client = Redis(
+            host=settings.redis_host,
+            port=settings.redis_port,
+            decode_responses=True,
+        )
+    return redis_client
+
+async def close_redis():
+    global redis_client
+    if redis_client:
+        await redis_client.close()
+        redis_client = None
