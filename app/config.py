b0e98d2f20c983b2d56b56c512d5d51050e692fb
Dwin Gharibi <dwin.gharibi@email.kntu.ac.ir>
2025-06-11 12:28:26 +0330

    fix(app/config.py): reordering app/config.py

--- app/config.py
+++ app/config.py
@@ -2,11 +2,11 @@ import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    mysql_host: str = os.getenv("MYSQL_HOST")
-    mysql_user: str = os.getenv("MYSQL_USER")
-    mysql_port: int = os.getenv("MYSQL_PORT")
-    mysql_password: str = os.getenv("MYSQL_PASSWORD")
-    mysql_db: str = os.getenv("MYSQL_DB")
+    mysql_host: str = os.getenv("MYSQL_HOST", "")
+    mysql_user: str = os.getenv("MYSQL_USER", "")
+    mysql_port: int = os.getenv("MYSQL_PORT", "")
+    mysql_password: str = os.getenv("MYSQL_PASSWORD", "")
+    mysql_db: str = os.getenv("MYSQL_DB", "")
 
     redis_url: str = os.getenv("REDIS_URL", "redis://localhost:6379/0")
     celery_broker_url: str = redis_url
@@ -19,17 +19,17 @@ class Settings(BaseSettings):
     mail_from_address: str = os.getenv("MAIL_FROM_ADDRESS", "")
     mail_form_name: str = os.getenv("MAIL_FROM_NAME", "")
 
-    redis_host: str = os.getenv("REDIS_HOST")
-    redis_port: int = int(os.getenv("REDIS_PORT"))
-    redis_password: str = os.getenv("REDIS_PASSWORD")
-    redis_db: int = int(os.getenv("REDIS_DB"))
+    redis_host: str = os.getenv("REDIS_HOST", "")
+    redis_port: int = int(os.getenv("REDIS_PORT", "30709"))
+    redis_password: str = os.getenv("REDIS_PASSWORD", "")
+    redis_db: int = int(os.getenv("REDIS_DB", "0"))
 
     jwt_expiration_minutes: int = int(os.getenv("JWT_EXPIRATION_MINUTES", "60"))
-    jwt_secret: str = os.getenv("JWT_SECTET")
-    jwt_algorithm: str = os.getenv("JWT_ALGORITHM")
+    jwt_secret: str = os.getenv("JWT_SECTET", "")
+    jwt_algorithm: str = os.getenv("JWT_ALGORITHM", "")
 
-    sms_ir_api_key: str = os.getenv("SMS_IR_API_KEY")
-    sms_ir_line_number: str = os.getenv("SMS_IR_LINE_NUMBER")
+    sms_ir_api_key: str = os.getenv("SMS_IR_API_KEY", "")
+    sms_ir_line_number: str = os.getenv("SMS_IR_LINE_NUMBER", "")
 
     otp_expiration_seconds: int = 300
 
