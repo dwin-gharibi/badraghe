a7605fcf731af50db62c52ddf0e95aeaaace3f3c
Dwin Gharibi <dwin.gharibi@email.kntu.ac.ir>
2025-06-11 14:50:44 +0330

    fix(openresty/scripts/shard.lua): adding some log flags to monitor problem

--- openresty/scripts/shard.lua
+++ openresty/scripts/shard.lua
@@ -9,25 +9,32 @@ local backend_nodes = {
 }
 
 local function get_start_index()
+    ngx.log(ngx.ERR, "[RR] Attempting to get start index")
+
     local lock_obj, err = lock:new("rr_counter")
     if not lock_obj then
-        ngx.log(ngx.ERR, "Failed to create lock: ", err)
+        ngx.log(ngx.ERR, "[RR] Failed to create lock: ", err)
         return 1
     end
 
     local elapsed, err = lock_obj:lock("backend_lock")
     if not elapsed then
-        ngx.log(ngx.ERR, "Failed to acquire lock: ", err)
+        ngx.log(ngx.ERR, "[RR] Failed to acquire lock: ", err)
         return 1
     end
 
     local counter = rr_dict:get("counter") or 0
+    ngx.log(ngx.ERR, "[RR] Current counter: ", counter)
+
     local index = (counter % #backend_nodes) + 1
     rr_dict:set("counter", counter + 1)
+    ngx.log(ngx.ERR, "[RR] New index: ", index, " Updated counter: ", counter + 1)
 
     local ok, unlock_err = lock_obj:unlock()
     if not ok then
-        ngx.log(ngx.ERR, "Failed to unlock: ", unlock_err)
+        ngx.log(ngx.ERR, "[RR] Failed to unlock: ", unlock_err)
+    else
+        ngx.log(ngx.ERR, "[RR] Lock released")
     end
 
     return index
@@ -36,19 +43,31 @@ end
 local function proxy_with_fallback()
     local start_index = get_start_index()
     local total_nodes = #backend_nodes
+    ngx.log(ngx.ERR, "[Proxy] Start index: ", start_index)
 
     for i = 0, total_nodes - 1 do
         local index = ((start_index + i - 1) % total_nodes) + 1
         local node = backend_nodes[index]
-        local client = http.new()
+        ngx.log(ngx.ERR, "[Proxy] Trying backend #", index, ": ", node)
 
+        local client = http.new()
+        client:set_timeout(5000)
+
+        local body_data = ngx.req.get_body_data()
         local res, err = client:request_uri(node .. ngx.var.request_uri, {
             method = ngx.req.get_method(),
             headers = ngx.req.get_headers(),
-            body = ngx.req.get_body_data(),
+            body = body_data,
             keepalive_timeout = 60,
-            keepalive_pool = 10
+            keepalive_pool = 10,
+            ssl_verify = false
         })
+
+        if res then
+            ngx.log(ngx.ERR, "[Proxy] Response status from ", node, ": ", res.status)
+        else
+            ngx.log(ngx.ERR, "[Proxy] Request error to ", node, ": ", err)
+        end
 
         if res and res.status < 500 then
             ngx.status = res.status
@@ -56,14 +75,16 @@ local function proxy_with_fallback()
                 ngx.header[k] = v
             end
             ngx.say(res.body)
+            ngx.log(ngx.ERR, "[Proxy] Successfully proxied request to ", node)
             return
         else
-            ngx.log(ngx.ERR, "Failed to proxy to ", node, ": ", err or ("HTTP " .. (res and res.status or "nil")))
+            ngx.log(ngx.ERR, "[Proxy] Failed to proxy to ", node, ": ", err or ("HTTP " .. (res and res.status or "nil")))
         end
     end
 
     ngx.status = 502
     ngx.say("All backend nodes failed")
+    ngx.log(ngx.ERR, "[Proxy] All backend nodes failed")
 end
 
 proxy_with_fallback()
