9c9ec368924dfa67580b00c138a2ae86367ec297
Dwin Gharibi <dwin.gharibi@email.kntu.ac.ir>
2025-08-20 22:13:15 +0330

    fix(src/services/api.js): fixing badraghe api endpoint for front

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://api1.badraghe.dwin.codes',
   timeout: 600000,
   headers: {
     'Content-Type': 'application/json',
